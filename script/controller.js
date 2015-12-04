faf099c45188f907b97f74d99062d254512607c5
stuarttiedemann <stuarttiedemann@yahoo.com>
2015-12-04 17:07:42 -0500

    Added object to array in controller

--- script/controller.js
+++ script/controller.js
@@ -5,12 +5,12 @@ angular.module('myApp',[]).controller('myController',function($scope){
 		this.link = link
 	}
 	$scope.projects=[];
-	$scope.projects.push(new $scope.Project("Google Maps API","img/work/googlemaps.png","http://stuarttiedemann.com/googlemaps/"));
+	$scope.projects.push(new $scope.Project("Google Maps API","img/work/googlemaps.png","http://googlemapsapi.bitballoon.com/"));
 	$scope.projects.push(new $scope.Project("Plane Routes Angular & Google Maps API","img/work/planeroutes.png","http://stuarttiedemann.com/planeroutes/#/"));
 	$scope.projects.push(new $scope.Project("Open Movie Database API","img/work/openmoviedatabase.png","http://openmoviedatabase.bitballoon.com"));
 	$scope.projects.push(new $scope.Project("House Listings in Angular","img/work/houselistingangular.png","http://houselistingsinangular.bitballoon.com"));
-	$scope.projects.push(new $scope.Project("Yahoo Stocks API","img/work/stocks.png","http://stuarttiedemann/angular-stocks.git"));
-	$scope.projects.push(new $scope.Project("Weather App API","img/work/WeatherApp.png","http://stuarttiedemann.com/planeroutes/#/"));
+	$scope.projects.push(new $scope.Project("Yahoo Stocks API","img/work/stocks.png","http://stuarttiedemann.com/angular-stocks"));
+	$scope.projects.push(new $scope.Project("Weather App API","img/work/WeatherApp.png","http://weatherapi.bitballoon.com/"));
 	$scope.projects.push(new $scope.Project("Youtube Mockup in Jquery & Angular API","img/work/youtube.png","http://stuarttiedemann.com/YouTube--master/"));
 	
 });
