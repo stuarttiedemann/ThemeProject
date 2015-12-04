30b530f80831bda79824faedc1dc85d4bd005b48
stuarttiedemann <stuarttiedemann@yahoo.com>
2015-12-04 16:53:33 -0500

    updated links and images

--- script/controller.js
+++ script/controller.js
@@ -1,14 +1,16 @@
 angular.module('myApp',[]).controller('myController',function($scope){
-	$scope.Project = function (title,image){
+	$scope.Project = function (title,image,link){
 		this.title = title;
-		this.image = image;
+		this.image = image
+		this.link = link
 	}
 	$scope.projects=[];
-	$scope.projects.push(new $scope.Project("Google Maps API","img/work/googlemaps.png"));
-	$scope.projects.push(new $scope.Project("House Listings in Angular","img/work/houselistingangular.png"));
-	$scope.projects.push(new $scope.Project("Open Movie Database API","img/work/openmoviedatabase.png"));
-	$scope.projects.push(new $scope.Project("Yahoo Stocks API","img/work/stocks.png"));
-	$scope.projects.push(new $scope.Project("Weather App API","img/work/WeatherApp.png"));
-	$scope.projects.push(new $scope.Project("Youtube Mockup in Jquery & Angular API","img/work/youtube.png"));
-	// console.log("The value of projects is: "+$scope.projects);
+	$scope.projects.push(new $scope.Project("Google Maps API","img/work/googlemaps.png","http://stuarttiedemann.com/googlemaps/"));
+	$scope.projects.push(new $scope.Project("Plane Routes Angular & Google Maps API","img/work/planeroutes.png","http://stuarttiedemann.com/planeroutes/#/"));
+	$scope.projects.push(new $scope.Project("Open Movie Database API","img/work/openmoviedatabase.png","http://openmoviedatabase.bitballoon.com"));
+	$scope.projects.push(new $scope.Project("House Listings in Angular","img/work/houselistingangular.png","http://houselistingsinangular.bitballoon.com"));
+	$scope.projects.push(new $scope.Project("Yahoo Stocks API","img/work/stocks.png","http://stuarttiedemann/angular-stocks.git"));
+	$scope.projects.push(new $scope.Project("Weather App API","img/work/WeatherApp.png","http://stuarttiedemann.com/planeroutes/#/"));
+	$scope.projects.push(new $scope.Project("Youtube Mockup in Jquery & Angular API","img/work/youtube.png","http://stuarttiedemann.com/YouTube--master/"));
+	
 });
