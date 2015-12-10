75f8c64ff050471b3d95add87ea365e01f39f61a
stuarttiedemann <stuarttiedemann@yahoo.com>
2015-12-10 13:27:47 -0500

    updated link

--- script/controller.js
+++ script/controller.js
@@ -8,7 +8,7 @@ angular.module('myApp',[]).controller('myController',function($scope){
 	$scope.projects.push(new $scope.Project("Google Maps API","img/work/googlemaps.png","http://googlemapsapi.bitballoon.com/"));
 	$scope.projects.push(new $scope.Project("Plane Routes Angular & Google Maps API","img/work/planeroutes.png","http://stuarttiedemann.com/planeroutes/#/"));
 	$scope.projects.push(new $scope.Project("Open Movie Database API","img/work/openmoviedatabase.png","http://openmoviedatabase.bitballoon.com"));
-	$scope.projects.push(new $scope.Project("House Listings in Angular","img/work/houselistingangular.png","http://houselistingsinangular.bitballoon.com"));
+	$scope.projects.push(new $scope.Project("House Listings in Angular","img/work/houselistingangular.png","http://stuarttiedemann.com/houses/"));
 	$scope.projects.push(new $scope.Project("Yahoo Stocks API","img/work/stocks.png","http://stuarttiedemann.com/angular-stocks"));
 	$scope.projects.push(new $scope.Project("Weather App API","img/work/WeatherApp.png","http://stuarttiedemann.com/weather/"));
 	
