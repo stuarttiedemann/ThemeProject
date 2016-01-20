ff4b6369f90e5dcd95c41d4954928d8cad68e93e
stuarttiedemann <stuarttiedemann@yahoo.com>
2016-01-20 10:20:16 -0500

    Updated content and styling

--- script/controller.js
+++ script/controller.js
@@ -7,12 +7,12 @@ angular.module('myApp',[]).controller('myController',function($scope){
 	}
 	$scope.projects=[];
 	$scope.projects.push(new $scope.Project("Nodejs Coffee Ecommerce Site","img/work/coffeesite.png","http://stuartscoffee.herokuapp.com/#","An ecommerce site using Nodejs and Mongo db."))
-	$scope.projects.push(new $scope.Project("Google Maps API","img/work/googlemaps.png","http://googlemapsapi.bitballoon.com/","Use Google Maps API to find directions, lodging, and grocery stores for a selected location."));
+	$scope.projects.push(new $scope.Project("Google Maps API Integration","img/work/googlemaps.png","http://googlemapsapi.bitballoon.com/","Use Google Maps API to find directions, lodging, and grocery stores for a selected location."));
 	$scope.projects.push(new $scope.Project("Plane Routes Angular & Google Maps API","img/work/planeroutes.png","http://stuarttiedemann.com/planeroutes/#/","A project that uses AngularJS and Google Maps API to visually show the flight range of various planes from a selected departure point."));
 	$scope.projects.push(new $scope.Project("Open Movie Database API","img/work/openmoviedatabase.png","http://openmoviedatabase.bitballoon.com", "A project showing the most popular movies using the openmoviedatabase API.  User can also search for movies and actors"));
 	$scope.projects.push(new $scope.Project("House Listings in Angular","img/work/houselistingangular.png","http://stuarttiedemann.com/houses/", "Angular project that allows the user to edit and update house listings."));
-	$scope.projects.push(new $scope.Project("Yahoo Stocks API","img/work/stocks.png","http://stuarttiedemann.com/angular-stocks","Use Yahoo's API to pull and display stock data. "));
-	$scope.projects.push(new $scope.Project("Weather App API","img/work/WeatherApp.png","http://stuarttiedemann.com/weather/","Use OpenWeatherMap API to retrieve current conditions and five day forecast for any location."));
+	$scope.projects.push(new $scope.Project("Yahoo Stocks Using Yahoo Finance API","img/work/stocks.png","http://stuarttiedemann.com/angular-stocks","Use Yahoo's API to pull and display stock data. "));
+	$scope.projects.push(new $scope.Project("Weather App Using Open Weather API","img/work/WeatherApp.png","http://stuarttiedemann.com/weather/","Use OpenWeatherMap API to retrieve current conditions and five day forecast for any location."));
 	
 	
 });
