509948c9089e04368a96e69b957ab0e79e8f0a1d
stuarttiedemann <stuarttiedemann@yahoo.com>
2016-02-04 00:20:54 -0500

    More styling changes

--- script/controller.js
+++ script/controller.js
@@ -12,7 +12,7 @@ angular.module('myApp',[]).controller('myController',function($scope){
 	$scope.projects.push(new $scope.Project("Plane Routes Angular & Google Maps API","img/work/plane.png","http://stuarttiedemann.com/planeroutes/#/","A project that uses AngularJS and Google Maps API to visually show the flight range of various planes from a selected departure point."));
 	$scope.projects.push(new $scope.Project("Use the open Movie Database API","img/work/movie.png","http://openmoviedatabase.bitballoon.com", "A project showing the most popular movies using the openmoviedatabase API.  User can also search for movies and actors"));
 	$scope.projects.push(new $scope.Project("Yahoo Stocks Using Yahoo Finance API","img/work/stocks.png","http://stuarttiedemann.com/angular-stocks","Use Yahoo's API to pull and display stock data. "));
-	$scope.projects.push(new $scope.Project("Weather App Using Open Weather API","img/work/Weather.png","http://stuarttiedemann.com/weather/","Use OpenWeatherMap API to retrieve current conditions and five day forecast for any location."));
+	$scope.projects.push(new $scope.Project("Weather App Using Open Weather API","img/work/weather.png","http://stuarttiedemann.com/weather/","Use OpenWeatherMap API to retrieve current conditions and five day forecast for any location."));
 	
 	
 });
